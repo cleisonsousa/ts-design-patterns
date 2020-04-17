7fef36c5a65ce8a06d158e3a4cf9fb1c96550b9e
Cleison Sousa <cleison.soussa15@gmail.com>
2020-04-17 15:48:17 -0300

    feat: add types

--- creational-patterns/singletonClient.ts
+++ creational-patterns/singletonClient.ts
@@ -1,9 +1,9 @@
 import Database from './singleton/database'
 
 export class SingletonClient {
-  static execute () {
-    const s1 = Database.getInstance();
-    const s2 = Database.getInstance();
+  static execute (): void {
+    const s1: Database = Database.getInstance();
+    const s2: Database = Database.getInstance();
 
     if (s1 === s2) {
       console.log('Singleton works, both variables contain the same instance.');
